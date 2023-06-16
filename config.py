6cdfc624ae95085f68c87e0f58aec8870a055b6d
Irina Fisunova <irina.l.fisunova@gmail.com>
2023-06-16 12:31:58 +0300

    Start

---
+++ config.py
@@ -0,0 +1,8 @@
+base_url = 'https://petstore.swagger.io/v2'
+statuses=['available', 'pending', 'sold']
+headers = {
+    'accept': 'application/json',
+    'content-type': 'application/json',
+}
+
+id = '9000004546'
